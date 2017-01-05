f828bfcd972ed86ce5c5200d2c1b5492b44879e3
Ionut Popovici <ionut.c.popovici@gmail.com>
2017-01-05 18:17:13 +0200

    added stuff + specular on earth shader

--- Jacked/Assets/_ImportedPackages/EarthRendering Free/Earth.shader
+++ Jacked/Assets/_ImportedPackages/EarthRendering Free/Earth.shader
@@ -1,3 +1,6 @@
+// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+// Upgrade NOTE: replaced '_World2Object' with 'unity_WorldToObject'
+
 Shader "Planet"
 {
 	Properties 
@@ -7,11 +10,21 @@ Shader "Planet"
 		_AtmosphereMultiply ("Atmosphere Multiply", Range(1, 3)) = 1.5
 
 		_DiffuseTex("Diffuse", 2D) = "white" {}
+		_SpecularTex("Specular", 2D) = "white" {}
 		
 		_CloudAndNightTex("Cloud And Night", 2D) = "black" {}
 
 		_LightDir("Light Dir", Vector) = (-1,0,0,1)
 		_ShadowLift("Shadow Lift", Range(0,1)) = 0.01
+
+		_SpecColor ("Specular Material Color", Color) = (1,1,1,1) 
+		_Shininess ("Shininess", Float) = 10
+
+		_Contrast("Contrast", Range(0, 25)) = 0.5
+		_Lift("Lift", Range(-25,25)) = 0.5
+
+		_SpecularFadeContrast("SpecFadeContrast", Range(0, 25)) = 10
+		_SpecularFadeLift("SpecFadeLift", Range(-25, 25)) = -9.81
 	}
 
 	SubShader 
@@ -27,12 +40,20 @@ Shader "Planet"
 			#pragma fragment frag
 			
 			sampler2D _DiffuseTex;
+			sampler2D _SpecularTex;
 			sampler2D _CloudAndNightTex;
+
+			uniform float4 _SpecColor; 
+			uniform float _Shininess;
 
 			float4 _AtmosphereColor;
 			float _AtmospherePow;
 			float _AtmosphereMultiply;
 			float _ShadowLift;
+			float _Contrast;
+			float _Lift;
+			float _SpecularFadeContrast;
+			float _SpecularFadeLift;
 
 			float4 _LightDir;
 
@@ -50,6 +71,8 @@ Shader "Planet"
 				half diffuse		: TEXCOORD1;
 				half night			: TEXCOORD2;
 				half3 atmosphere	: TEXCOORD3;
+				float4 posWorld : TEXCOORD4;
+				float3 normalDir : TEXCOORD5;
 			};
 			
 			vertexOutput vert(vertexInput input) 
@@ -65,22 +88,63 @@ Shader "Planet"
 				half3 normalDir = input.normal;
 				output.atmosphere = output.diffuse * _AtmosphereColor.rgb * pow(1 - saturate(dot(viewDir, normalDir)), _AtmospherePow) * _AtmosphereMultiply;
 
+				float4x4 modelMatrix = unity_ObjectToWorld;
+				float4x4 modelMatrixInverse = unity_WorldToObject; 
+ 
+				output.posWorld = mul(modelMatrix, input.pos);
+				output.normalDir = normalize(mul(float4(input.normal, 0.0), modelMatrixInverse).xyz);
+
 				return output;
 			}
 
 			half4 frag(vertexOutput input) : Color
 			{
+				half4 result;
 				half3 colorSample = tex2D(_DiffuseTex, input.uv).rgb;
-
+				half specSample = 1- tex2D(_SpecularTex, input.uv).r;
 				half3 cloudAndNightSample = tex2D(_CloudAndNightTex, input.uv).rgb;
 				half3 nightSample = cloudAndNightSample.ggb;
 				half cloudSample = cloudAndNightSample.r;
-
-				half4 result;
 				
+				specSample = clamp((specSample - 0.5f) * (_Contrast) + 0.5f - _Lift, 0, 1);
+
 				result.rgb = (colorSample + cloudSample) * clamp(input.diffuse + _ShadowLift, 0, 1)  + input.atmosphere;
 				result.a = 1;
+
+				float3 normalDirection = normalize(input.normalDir);
+				float3 viewDirection = normalize(_WorldSpaceCameraPos - input.posWorld.xyz);
+				float3 lightDirection;
+				float attenuation;
+ 
+				if (0.0 == _LightDir.w) // directional light?
+				{
+				   attenuation = 1.0; // no attenuation
+				   lightDirection = normalize(_LightDir.xyz);
+				} 
+ 
+				float3 ambientLighting = UNITY_LIGHTMODEL_AMBIENT.rgb * result.rgb;
+				float3 diffuseReflection = attenuation * result.rgb * max(0.0, dot(normalDirection, lightDirection));
+				float3 specularReflection;
+				 
+				float dotedDir = dot(normalDirection, lightDirection);
+				
+				if (dotedDir < 0.0) // light source on the wrong side?
+				{
+				   specularReflection = float3(0.0, 0.0, 0.0); // no specular reflection
+				}
+				else // light source on the right side
+				{
+				   specularReflection = attenuation 
+					  * _SpecColor.rgb 
+					  * pow(max(0.0, dot(reflect(-lightDirection, normalDirection), viewDirection)), _Shininess);
+				}
+
+				float multr = clamp((dotedDir - 0.5f) * (_SpecularFadeContrast) + 0.5f - _SpecularFadeLift, 0, 1);
+				specularReflection *= specSample * multr;
+
+				result = float4(ambientLighting + diffuseReflection + specularReflection, 1.0);
 				return result;
+
 			}
 		ENDCG
 		}
---
+++ Jacked/Assets/__Jacked/Prototypes/ProcGen/Backdrop/PlanetGeneration/PlanetTextureGen.cs
@@ -0,0 +1,312 @@
+﻿using UnityEngine;
+using CoherentNoise;
+using System.Collections;
+using CoherentNoise.Generation.Fractal;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+using CoherentNoise.Generation;
+using CoherentNoise.Generation.Patterns;
+
+public class PlanetTextureGen : MonoBehaviour
+{
+    private int _horizontalResolution;
+
+    public int Seed;
+    public int VerticalResolution = 1024;
+
+    [Header ("Region Mapping") ]
+    public float DeepOceanLevel = 0.1f;
+    public float SeaLevel = 0.1f;
+    public float ShoreLevel = 0.15f;
+    public float GrassLevel = 0.3f;
+    public float ForestLevel = 0.7f;
+    public float RockLevel = 0.8f;
+
+    public bool ColorMapEnabled = true;
+
+    public PlanetType PlanetType = PlanetType.Earthlike;
+
+    [Header ("Color Mapping")]
+    public Color DeepOceanColor = new Color(0.561f, 0.737f, 0.561f);
+    public Color SeaColor = new Color(0.118f, 0.565f, 1.000f);
+    public Color ShoreColor = new Color(0.957f, 0.643f, 0.376f);
+    public Color GrassColor = new Color(0.196f, 0.804f, 0.196f);
+    public Color ForestColor = new Color(0.133f, 0.545f, 0.133f);
+    public Color RockColor = new Color(0.545f, 0.271f, 0.075f);
+    public Color IceColor = new Color(0.9f, 0.9f, 0.9f);
+
+    [Header ("Terrain RidgeNoise Settings")]
+    public float Frequency = 1f;
+    public float Exponent = 0.4f;
+    public float Gain = 2f;
+    public float Offset = 0.7f;
+    public float Lacunarity = 0.5f;
+    public int OctaveCount = 6;
+
+    public float TurbulentFrequency = 4;
+    public float TurbulentPower = 0.2f;
+    public int TurbulentSeed = 2;
+
+    [Header ("Gas Giant")]
+    public float GasTurbulentFrequency = 4;
+    public float GasTurbulentPower = 0.2f;
+    public int GasTurbulentSeed = 2;
+
+    [Header ("[EXPERIMENTAL] Polar Caps Settings")]
+    public bool HasIceCaps = true;
+
+    [Range(1, 2)]
+    public float PolarExtent = 1.7f;
+    public float PolarFrequency = 1f;
+    public float PolarLacunarity = 0.5f;
+    public int PolarOctaveCount = 6;
+    public float PolarPersistance = 0.5f;
+    public float IceCapBias = 0.5f;
+    public float PolarTurbulence = 4f;
+    public float PolarTurbulencePower = 0.2f;
+    public int PolarTurbulenceSeed = 2;
+
+
+    public bool DoUpdate;
+
+    private void Start()
+    {
+        Generate();
+    }
+
+    public void Update()
+    {
+        if (DoUpdate)
+        {
+            Generate();
+            DoUpdate = false;
+        }
+    }
+
+    private struct ThreadArgs
+    {
+        public Color[] pixels;
+        public int i;
+        internal int memPerThread;
+        internal int width;
+        internal int height;
+        internal Color[] rawPx;
+    }
+
+    private void Generate()
+    {
+        var timeNow = Time.time;
+        _horizontalResolution = VerticalResolution * 2;
+
+
+        int numThreads = 8;
+
+        Color[] rawpixels = new Color[_horizontalResolution * VerticalResolution];
+        Color[] mappedpixels = new Color[_horizontalResolution * VerticalResolution];
+
+        Texture2D hMap = new Texture2D(_horizontalResolution, VerticalResolution, TextureFormat.RGB24, false);
+        Texture2D colorMap = new Texture2D(_horizontalResolution, VerticalResolution, TextureFormat.RGB24, false);
+
+        float uInc = 1.0f / (float)_horizontalResolution;
+        float vInc = 1.0f / (float)VerticalResolution;
+
+        int memPerThread = (_horizontalResolution * VerticalResolution) / numThreads;
+        Thread[] thrs = new Thread[numThreads];
+        for (int t = 0; t < numThreads; t++)
+        {
+            var thr = new Thread((arg) =>
+            {
+                switch (PlanetType)
+                {
+                    case PlanetType.Earthlike:
+                        GenerateEarthlike(arg);
+                        break;
+                    case PlanetType.GasGiant:
+                        GenerateGasGiant(arg);
+                        break;
+                    default:
+                        break;
+                }
+
+            });
+
+            thrs[t] = thr;
+            ThreadArgs args = new ThreadArgs()
+            {
+                i = t,
+                pixels = mappedpixels,
+                rawPx = rawpixels,
+                memPerThread = memPerThread,
+                width = _horizontalResolution,
+                height = VerticalResolution
+            };
+
+            thr.Start(args);
+        }
+
+        foreach (var x in thrs)
+        {
+            x.Join();
+        }
+
+        colorMap.SetPixels(mappedpixels);
+        colorMap.Apply();
+
+        hMap.SetPixels(rawpixels);
+        hMap.Apply();
+
+        GetComponent<Renderer>().material.SetTexture("_DiffuseTex", colorMap);
+        GetComponent<Renderer>().material.SetTexture("_SpecularTex", hMap);
+        GetComponent<Renderer>().material.mainTexture = colorMap;
+
+        var elapsed = Time.time - timeNow;
+        Debug.Log("Generated in " + elapsed + " seconds");
+    }
+
+    private void GenerateEarthlike(object arg)
+    {
+        var landmassGenerator = new RidgeNoise(Seed)
+        {
+            Frequency = Frequency,
+            Exponent = Exponent,
+            Gain = Gain,
+            Offset = Offset,
+            Lacunarity = Lacunarity,
+            OctaveCount = OctaveCount
+        }.Turbulence(TurbulentFrequency, TurbulentPower, TurbulentSeed);
+
+        var polarCapsGenerator = new Function((x, y, z) => Helpers.Saw(z / PolarExtent))
+            .Turbulence(PolarTurbulence, PolarTurbulencePower, PolarTurbulenceSeed)
+            .Gain(0.9f);
+
+        var qargs = (ThreadArgs)arg;
+        var i = qargs.i;
+        var pxls = qargs.pixels;
+
+        var starPos = qargs.memPerThread * i;
+        for (int q = starPos; q < qargs.memPerThread + starPos; q++)
+        {
+            var y = q / qargs.width;
+            var x = q - y * qargs.width;
+
+            var u = (float)x / (float)qargs.width;
+            var v = (float)y / (float)qargs.height;
+
+            Color targetColor = Color.clear;
+
+            var sphericalPos = MapUV(u, v);
+
+
+            var landSample = landmassGenerator.GetValue(sphericalPos);
+            qargs.rawPx[q] = new Color(landSample, landSample, landSample);
+
+            if (ColorMapEnabled)
+                targetColor = MapLandColor(landSample);
+            else
+                targetColor = qargs.rawPx[q];
+
+            if (HasIceCaps)
+            {
+                var iceCapSample = Mathf.Clamp(polarCapsGenerator.GetValue(sphericalPos), 0, 1);
+
+                Color iceColor = new Color(iceCapSample,iceCapSample,iceCapSample);
+
+                var a = targetColor;
+                var b = iceColor;
+                var one = Color.white;
+
+                targetColor = one - (one - a) * (one - b); // screen blend mode
+
+            }
+
+            pxls[q] = targetColor;
+        }
+    }
+
+    private void GenerateGasGiant(object arg)
+    {
+        var gasGenerator = new Function((x, y, z) => Mathf.Abs(Helpers.Saw(z / 0.9f)))
+            .Turbulence(GasTurbulentFrequency, GasTurbulentPower, GasTurbulentSeed)
+            .Gain(0.4f);
+
+        var qargs = (ThreadArgs)arg;
+        var i = qargs.i;
+        var pxls = qargs.pixels;
+
+        var starPos = qargs.memPerThread * i;
+        for (int q = starPos; q < qargs.memPerThread + starPos; q++)
+        {
+            var y = q / qargs.width;
+            var x = q - y * qargs.width;
+
+            var u = (float)x / (float)qargs.width;
+            var v = (float)y / (float)qargs.height;
+
+            Color targetColor = Color.clear;
+
+            var sphericalPos = MapUV(u, v);
+
+
+            var landSample = gasGenerator.GetValue(sphericalPos);
+
+            if (ColorMapEnabled)
+                targetColor = MapLandColor(landSample);
+            else
+                targetColor = new Color(landSample, landSample, landSample);
+
+            pxls[q] = targetColor;
+        }
+    }
+
+
+    private Color MapLandColor(float landSample)
+    {
+        Color landColor;
+        if (landSample < DeepOceanLevel)
+            landColor = DeepOceanColor;
+        else
+                if (landSample < SeaLevel)
+            landColor = InterpolateColor(DeepOceanLevel, SeaLevel, landSample, DeepOceanColor, SeaColor);
+        else
+                if (landSample < ShoreLevel)
+            landColor = InterpolateColor(SeaLevel, ShoreLevel, landSample, SeaColor, ShoreColor);
+        else
+                if (landSample < GrassLevel)
+            landColor = InterpolateColor(ShoreLevel, GrassLevel, landSample, ShoreColor, GrassColor);
+        else
+                if (landSample < ForestLevel)
+            landColor = InterpolateColor(GrassLevel, ForestLevel, landSample, GrassColor, ForestColor);
+        else
+                if (landSample < RockLevel)
+            landColor = InterpolateColor(ForestLevel, RockLevel, landSample, ForestColor, RockColor);
+        else
+            landColor = InterpolateColor(RockLevel, 1.0f, landSample, RockColor, IceColor);
+        return landColor;
+    }
+
+    private Color InterpolateColor(float sampleMin, float sampleMax, float sample, Color min, Color max)
+    {
+        var t = Mathf.InverseLerp(sampleMin, sampleMax, sample);
+        return Color.Lerp(min, max, t);
+    }
+
+    private Vector3 MapUV(float u, float v)
+    {
+        var azimuth = 2 * Mathf.PI * u;
+        var inclination = Mathf.PI * v;
+        var x = Mathf.Sin(inclination) * Mathf.Cos(azimuth);
+        var y = Mathf.Sin(inclination) * Mathf.Sin(azimuth);
+        var z = Mathf.Cos(inclination);
+
+        return new Vector3(x, y, z);
+    }
+
+
+}
+
+public enum PlanetType
+{
+    Earthlike,
+    GasGiant
+}
